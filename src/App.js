fc5e0c8aaa7a0606719158025d757f9f1fc359a5
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-11 17:05:04 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 
-function Square({value}){
-  function handleClick(){
-    console.log("clicked");
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('x');
   }
   return <button className='square' onClick={handleClick}>{value}</button>
 }
-
 
 
 export default function Board() {
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
     </React.Fragment>
