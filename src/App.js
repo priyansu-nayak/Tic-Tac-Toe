3628d781838ea478259fa10455538a0f1ecfd207
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-12 00:16:02 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function calculateWinner(squares) {
 }
 
 
-function Board({xIsNext,squares,onPlay}) {
+function Board({ xIsNext, squares, onPlay }) {
 
   /*
   Array(9) means creates array of size 9
@@ -54,9 +54,10 @@ function Board({xIsNext,squares,onPlay}) {
     else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+
   }
+
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -94,20 +95,21 @@ function Board({xIsNext,squares,onPlay}) {
 
 
 export default function Game() {
-  
-  const [xIsNext,setXIsNext]=useState(true);
-  const [history, setHistory] =useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length -1];
 
-  function handlePlay (nextSquares){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
 
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
 
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol></ol>
