a30c0418c5a70a3e310b55941bd76fa54264ae8c
priyansu-nayak <priyansu18.8@gmail.com>
2023-07-11 16:56:52 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-function Square(){
-  return <button className='square'>X</button>
+function Square({value}){
+  function handleClick(){
+    console.log("clicked");
+  }
+  return <button className='square'>{value}</button>
 }
 
 
@@ -10,21 +13,21 @@ export default function Board() {
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
 
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
 
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
 
     </React.Fragment>
